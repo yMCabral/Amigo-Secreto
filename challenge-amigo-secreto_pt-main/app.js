458411752bf6fa941a5c9eb2ae57a816e7e682d7
Emilly Cabral <emillycabral186@gmaul.com>
2025-02-26 21:48:42 -0300

    projeto inicial

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,11 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+let armazenarNomes = (listaDeNomes);
+let nomeSorteado = sortearNomeAleatorio;
+
+function sortearNomeAleatorio() {
+    let nomeSorteado = listaDeNomes [Math.floor(Math.random() * listaDeNomes.length)];
+    console.log (listaDeNomes);
+}
+//após colocar o nome
+
